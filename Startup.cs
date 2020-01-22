ece9ed06e316294aa223761151f6825e3ea259e5
Brubaker <mb997315@ohio.edu>
2020-01-22 10:16:35 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrubakerMIS4200.Startup))]
+namespace BrubakerMIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
